4f92cfae30ef9bb75633a39d47280fb7900b6d08
Tyler Ryden <tryden11@gmail.com>
2023-04-05 06:22:20 -0400

    WebView loading state added!

--- app/src/main/java/com/tryden/deltanews/DetailActivity.kt
+++ app/src/main/java/com/tryden/deltanews/DetailActivity.kt
@@ -2,6 +2,8 @@ package com.tryden.deltanews
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.databinding.DataBindingUtil
 import com.tryden.deltanews.databinding.ActivityDetailBinding
 
@@ -17,7 +19,14 @@ class DetailActivity : AppCompatActivity() {
         val binding: ActivityDetailBinding =
             DataBindingUtil.setContentView(this, R.layout.activity_detail)
 
-        val url = intent.getStringExtra(ARG_URL) ?: ""
-        binding.url = url
+        val client = object : WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                binding.contentLoadingProgressBar.hide()
+            }
+        }
+
+        binding.webView.webViewClient = client
+
+        binding.url = intent.getStringExtra(ARG_URL) ?: ""
     }
 }
